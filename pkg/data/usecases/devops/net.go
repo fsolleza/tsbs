a97fc0c02bea62502a4839a6393ef41346ca2d05
Franco Solleza <iam@francosolleza.com>
2021-01-20 00:12:13 -0500

    adding randomized IP address to simulate high-cardinality item

--- pkg/data/usecases/devops/net.go
+++ pkg/data/usecases/devops/net.go
@@ -11,6 +11,7 @@ import (
 var (
 	labelNet             = []byte("net") // heap optimization
 	labelNetTagInterface = []byte("interface")
+	labelNetTagIp        = []byte("ip-address")
 
 	// Reuse NormalDistributions as arguments to other distributions. This is
 	// safe to do because the higher-level distribution advances the ND and
@@ -33,18 +34,29 @@ var (
 type NetMeasurement struct {
 	*common.SubsystemMeasurement
 	interfaceName string
+    ipAddress string
+}
+
+func randomIp() string {
+    return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256))
 }
 
 func NewNetMeasurement(start time.Time) *NetMeasurement {
 	sub := common.NewSubsystemMeasurementWithDistributionMakers(start, netFields)
 	interfaceName := fmt.Sprintf("eth%d", rand.Intn(4))
+    ipAddress := randomIp()
 	return &NetMeasurement{
 		SubsystemMeasurement: sub,
 		interfaceName:        interfaceName,
+        ipAddress:            ipAddress,
 	}
 }
 
 func (m *NetMeasurement) ToPoint(p *data.Point) {
 	m.ToPointAllInt64(p, labelNet, netFields)
 	p.AppendTag(labelNetTagInterface, m.interfaceName)
+	p.AppendTag(labelNetTagIp, m.ipAddress)
+    if rand.Float64() < 0.2 {
+        m.ipAddress = randomIp()
+    }
 }
